8363e2458c1e1171f0a5bcddc29c24c27a5e2f12
Seggan <seggan21@gmail.com>
2026-04-17 16:14:51 -0400

    Woops

--- src/plugin.ts
+++ src/plugin.ts
@@ -51,14 +51,14 @@ async function onLoad() {
                 },
                 template: `
                 <div style="display:flex;flex-direction:column;height:100%">
-                    <p class="format_description">TODO</p>
+                    <p class="format_description">Rebar item display block</p>
                     <p class="format_target"><b>Target</b> : <span>Minecraft: Java Edition (Plugins)</span> <span>Plugin Developers</span> <span>Server Owners</span></p>
                     <content>
                         <h3 class="markdown">Good to know:</h3>
                         <p class="markdown">
                             <ul>
                                 <li>You need an internet connection to use this, its required to fetch the models and textures necessary to function!</li>
-                                <li>This format is designed to turn cuboid models into code loadable using TODO</li>
+                                <li>This format is designed to turn cuboid models into code loadable using Rebar</li>
                                 <li>A cuboid with a size of 1, 1, 1 is the size of the default minecraft block</li>
                                 <li>You can change a cuboids material by right clicking and selecting change material!</li>
                             </ul>
